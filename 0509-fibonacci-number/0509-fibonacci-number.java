b008e41c98492628f799c68c0223450d1b2ede5b
keerthireddy1021 <keerthireddy6103@gmail.com>
2025-06-12 14:00:51 +0530

    Time: 0 ms (100%), Space: 40.5 MB (39.55%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,21 +1,9 @@
-import java.util.Scanner;
-
 class Solution {
-    public static int fib(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        return fib(n - 1) + fib(n - 2);
-    }
-
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of terms: ");
-        int n = sc.nextInt();
-        System.out.println("Fibonacci Series:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(fib(i) + " ");
-        }
-        sc.close();
+    int[] dp = new int[31];
+    public int fib(int n) {
+        if (n <= 1) return n;
+        if (dp[n] != 0) return dp[n];
+        dp[n] = fib(n - 1) + fib(n - 2);
+        return dp[n];
     }
 }
